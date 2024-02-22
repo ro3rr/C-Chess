902adf80fb3a576be29ac01e54bbab82238eb297
ro3rr <123940788+ro3rr@users.noreply.github.com>
2024-02-22 19:21:32 +0100

    pluh2

---
+++ chess.py
@@ -0,0 +1,30 @@
+blank = 0
+pawn = 1
+knight = 2
+bishop = 3
+rook = 4
+queen = 5
+king = 6
+
+
+chessboard1 = ["p","p","p","p","p","p","p","p"]
+chessboard2 = ["r","n","b","q","k","b","n","r"]
+
+chessboardFull = [chessboard1, chessboard2]
+
+for x in chessboardFull:
+    print(x)
+
+
+
+
+
+
+#chessboard1 = [a1,b1,c1,d1,e1,f1,g1,h1]
+#chessboard2 = [a2,b2,c2,d2,e2,f2,g2,h2]
+#chessboard3 = [a3,b3,c3,d3,e3,f3,g3,h3]
+#chessboard4 = [a4,b4,c4,d4,e4,f4,g4,h4]
+#chessboard5 = [a5,b5,c5,d5,e5,f5,g5,h5]
+#chessboard6 = [a6,b6,c6,d6,e6,f6,g6,h6]
+#chessboard7 = [a7,b7,c7,d7,e7,f7,g7,h7]
+#chessboard8 = [a8,b8,c8,d8,e8,f8,g8,h8]
